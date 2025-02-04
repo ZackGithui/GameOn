6116dc755479cf8772f5275da649065a90c61a42
Zack Githui <jackszack177@gmail.com>
2025-02-04 22:46:48 +0300

    navigation updates

--- app/src/main/java/com/example/gameon/MainActivity.kt
+++ app/src/main/java/com/example/gameon/MainActivity.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
+import com.example.gameon.presentation.components.BottomNavigationBar
 import com.example.gameon.presentation.favourite.FavouriteScreen
 import com.example.gameon.presentation.game.Game
 import com.example.gameon.presentation.homeScreen.HomeScreen
@@ -26,14 +29,19 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             GameOnTheme {
-                App()
+                val navController = rememberNavController()
+                Scaffold(bottomBar = {
+                    BottomNavigationBar(navController = navController)
+                }) { paddingValues ->
+                    App(paddingValues)
+                }
             }
         }
     }
 }
 
 @Composable
-fun App() {
+fun App(paddingValues: PaddingValues) {
     val navController = rememberNavController()
     NavHost(navController = navController, startDestination = AppScreens.HomeScreens.route) {
         composable(AppScreens.HomeScreens.route) {
---
+++ app/src/main/java/com/example/gameon/presentation/navigation/BottomNavigation.kt
@@ -0,0 +1,54 @@
+package com.example.gameon.presentation.navigation
+
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun BottomNavigationBar(navController: NavController) {
+    val items = listOf(
+        AppScreens.HomeScreens,
+        AppScreens.SearchScreens,
+        AppScreens.FavouriteScreens
+    )
+    BottomAppBar {
+        val currentRoute = navController.currentBackStackEntry?.destination?.route
+        items.forEach { item ->
+            NavigationBarItem(
+                selected = currentRoute == item.route,
+                onClick = {
+                    navController.navigate(AppScreens.HomeScreens.route) {
+                        popUpTo(navController.graph.startDestinationId)
+                        launchSingleTop = true
+                    }
+                },
+                icon = { item.icon },
+                label = { Text(text = item.title) }
+            )
+            NavigationBarItem(
+                selected = currentRoute == item.route,
+                onClick = {
+                    navController.navigate(AppScreens.SearchScreens.route) {
+                        popUpTo(navController.graph.startDestinationId)
+                        launchSingleTop = true
+                    }
+                },
+                icon = { item.icon },
+                label = { Text(text = item.title) }
+            )
+            NavigationBarItem(
+                selected = currentRoute == item.route,
+                onClick = {
+                    navController.navigate(AppScreens.FavouriteScreens.route) {
+                        popUpTo(navController.graph.startDestinationId)
+                        launchSingleTop = true
+                    }
+                },
+                icon = { item.icon },
+                label = { Text(text = item.title) }
+            )
+        }
+    }
+}
